2df6e3fc7a367db2331952fea7ef479ff818a6f3
Mark Madi <markmadi232@gmail.com>
2023-12-21 15:39:37 +0200

    Fixed Navbar spacing and reponsiveness issues

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,15 +1,47 @@
+"use client";
+
 import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Item from "@mui/material/ListItem";
 import Link from "next/link";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  textAlign: "center",
+  boxShadow: "none",
+  border: "5px solid black",
+}));
+
+const Item2 = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  textAlign: "left",
+  boxShadow: "none",
+}));
+
+const FavouriteItem = styled(Paper)(() => ({
+  textAlign: "left",
+  boxShadow: "none",
+}));
+
+const DynamicItem = styled(Paper)(() => ({
+  textAlign: "center",
+  position: "sticky",
+  boxShadow: "none"
+}));
+
+const LogoutItem = styled(Paper)(() => ({
+  textAlign: "right",
+  boxShadow: "none",
+}));
 
 const page = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={4}>
-          <Item className="itemHome itemGrid">
+          <FavouriteItem className="itemHome itemGrid">
             <div className="favouriteButton">
               <Link
                 style={{
@@ -22,46 +54,73 @@ const page = () => {
                 Favourites
               </Link>
             </div>
-          </Item>
+          </FavouriteItem>
         </Grid>
         <Grid item xs={4}>
-          <Item className="itemHome itemGrid">
-
+          {/* <DynamicItem className="DynamicItem">
+            <Box display="flex" justifyContent="center" alignItems="center">
               <div className="dynamicIsland">
                 <h1
-                  className="userName"
                   style={{
                     color: "white",
-                    fontSize: "19px",
-                    paddingLeft: "1em",
+                    fontSize: "20px",
+                    textAlign: "left",
                   }}
+                  className="userName"
                 >
                   Mark M.
                 </h1>
-                <div className="profileIcon"></div>
               </div>
-
-          </Item>
+              <div className="profileIcon"></div>
+            </Box>
+          </DynamicItem> */}
         </Grid>
         <Grid item xs={4}>
-          <Item className="itemHome itemGrid">
+          <LogoutItem className="itemHome itemGrid">
             <div className="logoutButton">
               <Link
                 style={{
                   textDecoration: "none",
                   color: "black",
                   fontWeight: "600",
-                  position: "relative",
-                  left:"20em"
                 }}
-                href="/login"
+                href="/"
               >
                 Log Out
               </Link>
             </div>
-          </Item>
+          </LogoutItem>
         </Grid>
-
+        <Grid item xs={8}>
+          <Item>Grid</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>Grid</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>Grid</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>Grid</Item>
+        </Grid>
+        <Grid item xs={8}>
+        <Item2>
+          <div class="search">
+    <div>
+      <input type="text" placeholder="Search . . ." required/>
+    </div>
+  </div>
+          </Item2>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>Grid</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>Grid</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>Grid</Item>
+        </Grid>
       </Grid>
     </Box>
   );
